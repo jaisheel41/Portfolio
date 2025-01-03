6ed798d72e07d6babc7ab85299e8dbf8f99d9793
jaisheel41 <jaisheel42@gmail.com>
2025-01-03 18:01:08 +0000

    media inclusion

--- backend/portfolio_backend/settings.py
+++ backend/portfolio_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,3 +133,6 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'backend/projects/')
