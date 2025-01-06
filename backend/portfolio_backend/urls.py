0b806203524d7f7199a57a18db9f91bddd7ca4cb
jaisheel41 <jaisheel42@gmail.com>
2025-01-06 17:19:12 +0000

    education fixed

--- backend/portfolio_backend/urls.py
+++ backend/portfolio_backend/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
+from django.conf import settings
+from django.conf.urls.static import static
 
 def root_view(request):
     return JsonResponse({"message": "Welcome to the Portfolio API"})
@@ -26,3 +28,6 @@ urlpatterns = [
     path('api/', include('portfolio.urls')),
     path('', root_view),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
